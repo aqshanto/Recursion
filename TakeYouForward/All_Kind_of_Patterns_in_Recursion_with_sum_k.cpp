922a776970d9d645cb1ec76d5bd5df0a792ce7e8
aqShanto <abdulquymshanto@gmail.com>
2025-02-11 13:23:10 +0600

    up

--- TakeYouForward/All_Kind_of_Patterns_in_Recursion_with_sum_k.cpp
+++ TakeYouForward/All_Kind_of_Patterns_in_Recursion_with_sum_k.cpp
@@ -1,7 +1,63 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define endl "\n"
+// #define printarr(v)                    \
+//     for (int i = 0; i < v.size(); i++) \
+//         cout << v[i] << " ";           \
+//     cout << endl;
+
+// //---------------------------------------//
+
+// int n, sum;
+// void printPattern(int index, vector<int>& ds, vector<int>& v, int s) {
+//     if (index >= v.size()) {
+//         if (s == sum) {
+//             for (int i = 0; i < ds.size(); i++) {
+//                 cout << ds[i] << " ";
+//             }
+//             cout << endl;
+//         }
+//         return;
+//     }
+
+//     ds.push_back(v[index]);
+//     s += v[index];
+//     // taking the value
+//     printPattern(index + 1, ds, v, s);
+//     ds.pop_back();
+//     s -= v[index];
+//     // not taking the value
+//     printPattern(index + 1, ds, v, s);
+// }
+
+// void solve() {
+//     cin >> n >> sum;
+//     vector<int> v(n), ds;
+//     for (int i = 0; i < n; i++) cin >> v[i];
+//     printPattern(0, ds, v, 0);
+// }
+
+// signed main() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     int t = 1, Case = 1;
+//     // cin>>t;
+//     while (t--) {
+//         // cout << "Case " << Case++ << ": ";
+//         solve();
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define double long double
 #define endl "\n"
+#define inputarr(v, n) \
+    for (int i = 0; i < n; i++) cin >> v[i];
+
 #define printarr(v)                    \
     for (int i = 0; i < v.size(); i++) \
         cout << v[i] << " ";           \
@@ -9,33 +65,35 @@ using namespace std;
 
 //---------------------------------------//
 
-int n, sum;
-void printPattern(int index, vector<int>& ds, vector<int>& v, int s) {
-    if (index >= v.size()) {
+void printpattern(int i, vector<int> ds, vector<int> v, int n, int sum) {
+    if (i >= n) {
+        int s = 0;
+        for (int j = 0; j < ds.size(); j++) {
+            s += ds[j];
+        }
         if (s == sum) {
-            for (int i = 0; i < ds.size(); i++) {
-                cout << ds[i] << " ";
+            for (int j = 0; j < ds.size(); j++) {
+                cout << ds[j] << " ";
             }
             cout << endl;
         }
         return;
     }
-
-    ds.push_back(v[index]);
-    s += v[index];
-    // taking the value
-    printPattern(index + 1, ds, v, s);
+    ds.push_back(v[i]);
+    printpattern(i + 1, ds, v, n, sum);
     ds.pop_back();
-    s -= v[index];
-    // not taking the value
-    printPattern(index + 1, ds, v, s);
+    printpattern(i + 1, ds, v, n, sum);
 }
 
 void solve() {
-    cin >> n >> sum;
-    vector<int> v(n), ds;
+    int n;
+    cin >> n;
+    vector<int> v(n);
     for (int i = 0; i < n; i++) cin >> v[i];
-    printPattern(0, ds, v, 0);
+    int sum;
+    cin >> sum;
+    vector<int> ds;
+    printpattern(0, ds, v, n, sum);
 }
 
 signed main() {
--- TakeYouForward/Recursion_on_Subsequences.cpp
+++ TakeYouForward/Recursion_on_Subsequences.cpp
@@ -1,71 +1,122 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define endl "\n"
+// #define printarr(v)                    \
+//     for (int i = 0; i < v.size(); i++) \
+//         cout << v[i] << " ";           \
+//     cout << endl;
+
+// //---------------------------------------//
+// int n;
+
+// void pSub(int index, vector<int>& ds, vector<int>& v, int n) {
+//     if (index == n) {
+//         for (int i = 0; i < ds.size(); i++) {
+//             cout << ds[i] << " ";
+//         }
+//         if (ds.size() == 0) cout << "[]";
+//         cout << endl;
+//         return;
+//     }
+//     // take or pick the particular index into the subsequence
+//     ds.push_back(v[index]);
+//     pSub(index + 1, ds, v, n);
+
+//     // not take or pick the particular index into the subsequence
+//     ds.pop_back();
+//     pSub(index + 1, ds, v, n);
+// }
+
+// void psubRev(int index, vector<int>& ds, vector<int>& v, int n) {
+//     if (index == n) {
+//         for (int i = 0; i < ds.size(); i++) {
+//             cout << ds[i] << " ";
+//         }
+//         if (ds.size() == 0) cout << "[]";
+//         cout << endl;
+//         return;
+//     }
+//     // not take or pick the particular index into the subsequence
+//     pSub(index + 1, ds, v, n);
+
+//     // take or pick the particular index into the subsequence
+//     ds.push_back(v[index]);
+//     pSub(index + 1, ds, v, n);
+
+//     ds.pop_back();
+// }
+
+// void solve() {
+//     cin >> n;
+//     vector<int> v(n), ds;
+//     for (int i = 0; i < n; i++) cin >> v[i];
+//     pSub(0, ds, v, n);
+//     cout << endl
+//          << "IN REVERSE ORDER" << endl;
+
+//     // IN REVERSE ORDER
+//     vector<int> ds1;
+//     psubRev(0, ds1, v, n);
+// }
+
+// signed main() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     int t = 1;
+//     // cin>>t;
+//     while (t--) {
+//         solve();
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define double long double
 #define endl "\n"
+#define inputarr(v, n) \
+    for (int i = 0; i < n; i++) cin >> v[i];
+
 #define printarr(v)                    \
     for (int i = 0; i < v.size(); i++) \
         cout << v[i] << " ";           \
     cout << endl;
 
 //---------------------------------------//
-int n;
 
-void pSub(int index, vector<int>& ds, vector<int>& v, int n) {
-    if (index == n) {
-        for (int i = 0; i < ds.size(); i++) {
-            cout << ds[i] << " ";
+vector<int> ds;
+void printpattern(int i, vector<int> v, int n) {
+    if (i >= n) {
+        for (int j = 0; j < ds.size(); j++) {
+            cout << ds[j] << " ";
         }
         if (ds.size() == 0) cout << "[]";
         cout << endl;
         return;
     }
-    // take or pick the particular index into the subsequence
-    ds.push_back(v[index]);
-    pSub(index + 1, ds, v, n);
-
-    // not take or pick the particular index into the subsequence
-    ds.pop_back();
-    pSub(index + 1, ds, v, n);
-}
-
-void psubRev(int index, vector<int>& ds, vector<int>& v, int n) {
-    if (index == n) {
-        for (int i = 0; i < ds.size(); i++) {
-            cout << ds[i] << " ";
-        }
-        if (ds.size() == 0) cout << "[]";
-        cout << endl;
-        return;
-    }
-    // not take or pick the particular index into the subsequence
-    pSub(index + 1, ds, v, n);
-
-    // take or pick the particular index into the subsequence
-    ds.push_back(v[index]);
-    pSub(index + 1, ds, v, n);
-
+    printpattern(i + 1, v, n);
+    ds.push_back(v[i]);
+    printpattern(i + 1, v, n);
     ds.pop_back();
 }
 
 void solve() {
+    int n;
     cin >> n;
-    vector<int> v(n), ds;
+    vector<int> v(n);
     for (int i = 0; i < n; i++) cin >> v[i];
-    pSub(0, ds, v, n);
-    cout << endl
-         << "IN REVERSE ORDER" << endl;
-
-    // IN REVERSE ORDER
-    vector<int> ds1;
-    psubRev(0, ds1, v, n);
+    printpattern(0, v, n);
 }
 
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t = 1;
+    int t = 1, Case = 1;
     // cin>>t;
     while (t--) {
+        // cout << "Case " << Case++ << ": ";
         solve();
     }
     return 0;
