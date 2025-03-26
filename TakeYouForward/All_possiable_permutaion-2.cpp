2c9617d2ddefdbd4fe8bee39a9e393c16dcd8d7b
aqShanto <abdulquymshanto@gmail.com>
2025-03-26 16:26:28 +0600

    up

---
+++ TakeYouForward/All_possiable_permutaion-2.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define endl "\n"
+#define inputarr(v, n) \
+    for (int i = 0; i < n; i++) cin >> v[i];
+
+#define printarr(v)                    \
+    for (int i = 0; i < v.size(); i++) \
+        cout << v[i] << " ";           \
+    cout << endl;
+
+//---------------------------------------//
+
+void pp(vector<int>v,int b,int n){
+    if(b==n){
+        printarr(v);
+        return;
+    }
+    for(int i=b;i<n;i++){
+        swap(v[b],v[i]);
+        pp(v,b+1,n);
+        swap(v[b],v[i]);
+    }
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    pp(v,0,n);
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1, Case = 1;
+    // cin>>t;
+    while (t--) {
+        // cout << "Case " << Case++ << ": ";
+        solve();
+    }
+    return 0;
+}
